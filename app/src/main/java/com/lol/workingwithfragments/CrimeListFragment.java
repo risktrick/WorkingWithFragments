e86a022c9b5abd295420e92bb4a213d415aeab00
user <user@pc.com>
2016-04-29 12:57:27 +0300

    My own array adapter

--- app/src/main/java/com/lol/workingwithfragments/CrimeListFragment.java
+++ app/src/main/java/com/lol/workingwithfragments/CrimeListFragment.java
@@ -1,12 +1,19 @@
 package com.lol.workingwithfragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CrimeListFragment extends ListFragment {
 
@@ -20,7 +27,33 @@ public class CrimeListFragment extends ListFragment {
             modelArray.add(new Model());
         }
 
-        ArrayAdapter<Model> adapter = new ArrayAdapter<Model>(getActivity(), android.R.layout.simple_list_item_1, modelArray);
+        MyAdapter adapter = new MyAdapter(getActivity(), android.R.layout.simple_list_item_1, modelArray);
         setListAdapter(adapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Log.e("aaa", "click " + position);
+    }
+
+    private class MyAdapter extends ArrayAdapter<Model> {
+        public MyAdapter(Context context, int resource, List<Model> objects) {
+            super(context, resource, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            if(convertView == null){
+                convertView = getActivity().getLayoutInflater().inflate(R.layout.item_model, null);
+            }
+
+            Model model = getItem(position);
+
+            TextView textView = (TextView)convertView.findViewById(R.id.textViewItem);
+            textView.setText(":) " + model.uuid);
+
+            return convertView;
+        }
     }
 }
